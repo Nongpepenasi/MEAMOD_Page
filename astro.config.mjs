c503e267bf73a50e61bf492e5071d2b8fd6c5e26
Thanakorn Waleejaroenpong <tanakorn_wal@cmu.ac.th>
2025-02-26 13:54:51 +0700

    Fix config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import github from "@astrojs/github";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nongpepenasi.github.io",
+  site: "https://nongpepenasi.github.io/MEAMOD-Page",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
   base: "/MEAMOD-Page/",
+  output: "static",
+  adapter: github(),
 });
