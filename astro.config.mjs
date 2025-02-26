b8277f351d9819f5b92eb35245be2bd2c953c7a1
Thanakorn Waleejaroenpong <tanakorn_wal@cmu.ac.th>
2025-02-26 13:56:43 +0700

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
-import github from "@astrojs/github";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +10,4 @@ export default defineConfig({
   integrations: [tailwind(), mdx(), sitemap(), icon()],
   base: "/MEAMOD-Page/",
   output: "static",
-  adapter: github(),
 });
