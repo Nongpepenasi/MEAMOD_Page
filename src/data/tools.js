238608ba5d494ac4beeb8d3d4a5f43c886e1f1d7
Thanakorn Waleejaroenpong <tanakorn_wal@cmu.ac.th>
2025-02-27 14:53:18 +0700

    Fix project more button and show three lastest pro

---
+++ src/data/tools.js
@@ -0,0 +1,20 @@
+export const tools = [
+  {
+    title: "maemod R package",
+    url: "https://github.com/slphyx/maemod",
+    web: "https://slphyx.shinyapps.io/maemodshiny/",
+  },
+  {
+    title: "P. falciparum Dynamic models",
+    url: "https://github.com/slphyx/P.-falciparum-Dynamics",
+  },
+  {
+    title: "Hat Game: A Simple SIR model",
+    url: "https://slphyx.shinyapps.io/HatGame/",
+    code: "https://github.com/slphyx/hatgame",
+  },
+  {
+    title: "RandomApp",
+    url: "https://github.com/MAEMOD-MORU/RandomApp",
+  },
+];
